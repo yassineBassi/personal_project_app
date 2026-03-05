d566935132ca4dca0108b719ed2b8e404b44187c
BASSI Yassine <yassinebassii@gmail.com>
2026-03-05 07:51:13 +0000

    connect the api service to the database

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [ApiController],
   providers: [ApiService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(`${process.env.DATABASE_PORT}`, 10) || 5432,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // Disable in production
+    }),
+  ],
 })
 export class ApiModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,5 +6,10 @@ async function bootstrap() {
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`API service running on port ${port}`);
+  console.log("database name: ", process.env.DATABASE_NAME);
+  console.log("database host: ", process.env.DATABASE_HOST);
+  console.log("database port: ", process.env.DATABASE_PORT);
+  console.log("database username: ", process.env.DATABASE_USERNAME);
+  console.log("database password: ", process.env.DATABASE_PASSWORD);
 }
 bootstrap();
