cd7d91441bcfa7caea04eb492060eba5ab348e8b
BASSI Yassine <yassinebassii@gmail.com>
2026-03-07 07:47:36 +0000

    add logs to the api-service

--- apps/api/src/api.service.ts
+++ apps/api/src/api.service.ts
@@ -10,23 +10,36 @@ export class ApiService {
   constructor(@InjectRepository(Url) private urlsRepository: Repository<Url>){}
 
     async shortenURL(url: string) {
+      console.log("shorten url api");
+      
       const urlObject = {
         originalUrl: url,
         code: UUID(),
         clickCount: 0
       };
+      console.log("url object", urlObject);
+      
       await this.urlsRepository.save(urlObject);
+      console.log("saved");
+      
       return "done";
     }
   
     async getOriginalURL(code: string) {
+      console.log("get original url");
+      console.log("code is ", code);
+
       const urlObject = await this.urlsRepository.findOne({where: {code}});
+      console.log("url object is ", urlObject);
+
       if(!urlObject){
         throw new HttpException('URL not found', 404);
       }
+
       urlObject.clickCount += 1;
       this.urlsRepository.save(urlObject);
-      
+      console.log("url object click count", urlObject.clickCount)
+
       return urlObject.originalUrl;
     }
   
