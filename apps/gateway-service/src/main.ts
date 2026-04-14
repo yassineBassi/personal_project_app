7d1a9b125659041031ffbfea94c8fb24e4dbeb99
BASSI Yassine <yassinebassii@gmail.com>
2026-04-14 23:17:23 +0100

    gateway-service fix 304 status code in getOriginalCode route

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -3,7 +3,8 @@ import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  app.enableCors({ 
+  app.getHttpAdapter().getInstance().set('etag', false);
+  app.enableCors({
     origin: process.env.ORIGIN_URLs ? process.env.ORIGIN_URLs.split(',') : '*',
     methods: 'GET,HEAD,PUT,POST,OPTIONS',
     allowedHeaders: '*',
